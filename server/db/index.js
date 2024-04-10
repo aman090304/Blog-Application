129908065b602bf9fa91b2d53f8057902a635585
Aman Gupta <96950111+aman090304@users.noreply.github.com>
2024-04-10 19:32:27 +0530

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 
 mongoose
   .connect(
-    "mongodb+srv://amanrgupta456:DChTJngCco2QDxbL@iliyas.erdr6ow.mongodb.net/"
+    <YOUR CONNECTION STRING>
   )
   .then(() => console.log("Connected mongo db"))
   .catch((e) => console.log(e));
